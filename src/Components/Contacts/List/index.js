fbac87fe216029b6e2e8b1a56890e7f251402c36
Eray Alp <erayalp.e@gmail.com>
2023-06-19 16:13:29 +0300

    List added

---
+++ src/Components/Contacts/List/index.js
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+
+function List( {contacts} ) {
+  const [filterText, setFilterText] = useState("");
+
+  const filteredContacts = contacts.filter((item) => {
+    return Object.keys(item).some((key) => 
+    (
+      item[key]
+      .toString()
+      .toLowerCase()
+      .includes(filterText.toLocaleLowerCase())
+    ))
+  })
+
+  console.log("filtered", filteredContacts);
+
+  return (
+    <div>
+
+      <input 
+      placeholder='Filter Contact' 
+      value={filterText} 
+      onChange={(e) => setFilterText(e.target.value)}/>
+
+      <ul>
+        {filteredContacts.map((contact, index) => (<li key={index}>{contact.fullname}</li>))}
+      </ul>
+    </div>
+  )
+}
+
+export default List
